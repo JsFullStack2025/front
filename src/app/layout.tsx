b94ce1d1e2d0d12f0df9cd49e6d371a25f5e0dc3
IlyaA <ilya0000007@gmail.com>
2025-03-26 16:07:45 +0700

    background layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div className="bg-gradient-to-r from-primary-from to-primary-to flex flex-col items-center justify-center min-h-screen">
         {children}
+        </div>
       </body>
     </html>
   );
--- src/app/profset/page.tsx
+++ src/app/profset/page.tsx
@@ -1,6 +1,7 @@
 export default function profset() {
     return(
-        <div>
+        <div className="bg-indigo-500 ...">
+        
             hello
         </div>
     ) 
