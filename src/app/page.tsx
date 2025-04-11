8362c6dfe15e456703f344e67cf4dab61ea47cbd
Oleg Kulik <kulik.ol.val@gmail.com>
2025-04-11 11:52:45 +0300

    fetch to backend user profile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,19 +6,33 @@ import Image from "next/image";
 import { Button } from '@/components/ui/button'
 
 import testJWT from '@/lib/api/testjwt'
+import { GetUser } from "@/lib/api/user";
+import { useState } from 'react';
 
 export default function Home() {
 
+  const handleButtonClick = (action: string) => {
+    console.log(`Кнопка с действием "${action}" была нажата`);
+    if (action === 'action1') { 
+      const data = testJWT()
+      console.log(data)
+    }  
+    if (action === 'action2') {
+      const data = GetUser(2)
+      console.log(data)
+    }
+};
+/*
   const onClick = async () => {
     const data = await testJWT()
     console.log(data)
 }
-
+*/
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center  p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-      <Button onClick={onClick} type="button" variant="outline" size="sm">Send</Button>
-        
+      <Button  onClick={() => handleButtonClick('action1')} type="button" variant="outline" size="sm">Send</Button>
+      <Button  onClick={() => handleButtonClick('action2')} type="button" variant="outline" size="sm">Send</Button>
       </main>
     </div>
   );
--- src/lib/api/user.ts
+++ src/lib/api/user.ts
@@ -5,11 +5,13 @@ export async function GetUsers(){
     credentials: 'include', // Браузер автоматически добавит куки
     headers: {'Accept': 'application/json', 'content-type': 'application/json', },
   })
-  return res.json()
+  const data = await res.json()
+  //console.log(res)
+  return data
 }
 
 export async function GetUser(id: number){
-  const res = await fetch(process.env.BACKEND_URI + "/user" + id, {
+  const res = await fetch("http://localhost:3000/user" + id, {
     cache: 'no-store',
     method: "GET",
     credentials: 'include', // Браузер автоматически добавит куки
