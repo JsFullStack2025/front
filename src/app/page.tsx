57525c6fbf6d2325aadabff9ea24df0dd86964d4
Max-TeXnic <max.cyberia@gmail.com>
2025-04-13 21:15:32 +0800

    remove html

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
 
   return (
     <>
-    FAKE INITIAL PAGE
+    FAKE ROOT PAGE
     </>
   );
 }
--- src/app/sharedcard/[slug]/page.tsx
+++ src/app/sharedcard/[slug]/page.tsx
@@ -1,7 +1,4 @@
-export default function card(){
-    return (
-    <>
-        card work
-    </>
-    )
+export default async function Page({ params }: { params: Promise<{ slug: string }>}) {
+  const { slug } = await params
+  return <div>Shared card: {slug}</div>
 }
