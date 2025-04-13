77e7952e488f2ff7c0017d80c0838ff39e3b355f
Max-TeXnic <max.cyberia@gmail.com>
2025-04-13 22:02:54 +0800

    remove any ts

--- src/app/userprofile/[slug]/page.tsx
+++ src/app/userprofile/[slug]/page.tsx
@@ -5,14 +5,16 @@ import "./stylePageUP.css";
 
 import { Button } from '@/components/ui/button'
 
-import testFetch from '@/lib/api/test-fetch'
-
 export default function UserProfile() {
 
-     const onClick = async () => {
-         const data = await testFetch()
-         console.log(data)
+     const onSendClick = async () => {
+         //const data = await testFetch()
+         //console.log(data)
+         console.log('send clicked')
      }
+     // states profile projects cards
+     // use effect get data
+     // get profile projects cards
 
   return (
     <div className="h-screen font-sans  overflow-y-auto">
@@ -25,7 +27,7 @@ export default function UserProfile() {
                         <h2 className="text-xl text-center">Редактировать профиль</h2>
 
                     </div>
-                   <Button onClick={onClick} type="button" variant="outline" size="sm">Send</Button>
+                   <Button onClick={onSendClick} type="button" variant="outline" size="sm">Send</Button>
                     <div className="flex justify-center mb-6 gap-4 items-center ">
 
                         <Image className="w-32 h-32 rounded-full border-2 border-indigo-300" src="/img/foto.jpg"
@@ -79,13 +81,13 @@ export default function UserProfile() {
                     </div>
                     <ul className="p-6">
                         <li className="flex flex-row justify-between items-center h-min">
-                            <button className=""><svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                            {/* <button className=""><svg width="24" height="24" viewBox="0 0 24 24" fill="none"
                                     xmlns="http://www.w3.org/2000/svg">
                                     <path
                                         d="M7 21C6.45 21 5.97917 20.8042 5.5875 20.4125C5.19583 20.0208 5 19.55 5 19V6H4V4H9V3H15V4H20V6H19V19C19 19.55 18.8042 20.0208 18.4125 20.4125C18.0208 20.8042 17.55 21 17 21H7ZM17 6H7V19H17V6ZM9 17H11V8H9V17ZM13 17H15V8H13V17Z"
                                         fill="#49454F" />
                                 </svg>
-                            </button>
+                            </button> */}
                             <span className="grow mx-5">Проект 1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto blanditiis debitis nesciunt, dicta sapiente facere nulla aperiam animi qui nemo rem? Id, nam dolor fugit sed veniam optio mollitia a.</span>
                             <button className="text-blue-500"><svg width="24" height="24" viewBox="0 0 24 24" fill="none"
                                     xmlns="http://www.w3.org/2000/svg">
--- src/lib/api/test-fetch.ts
+++
@@ -1,5 +0,0 @@
-export default async function testFetch() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/todos/1")
-  const data = await res.json()
-  return data
-}
