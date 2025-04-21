26c7bf5c83529b47cef3a4b1543f77d561b40bad
Max-TeXnic <max.cyberia@gmail.com>
2025-04-21 18:05:09 +0800

    pre merge commit

--- src/app/cardtypes/[slug]/page.tsx
+++ src/app/cardtypes/[slug]/page.tsx
@@ -30,30 +30,21 @@ export default function Page({ params }: { params: Promise<{ slug: string }>}) {
         };
             fetchData();
     },[]);
-    const form = useForm();
+    const {register, handleSubmit} = useForm();
+    const onTitleChange = (e)=>{
+        console.log(e);
+    }
+
 
     return <div className="h-screen font-sans overflow-y-auto p-20">
     <div className="bg-white text-gray-800 rounded-lg w-full sm:h-3/4 sm:min-w-xl overflow-y-auto mb-2 sm:mb-0">
         <div className="flex items-center rounded-t-lg  h-15 px-8 bg-header-project-list">
             <h3 className="text-xl text-white">{cardType?.title}</h3>
         </div>
-        <Form {...form}>
-<FormField
-                                        control={form.control}
-                                        name="title"
-                                        render={({ field }) => (
-                                            <FormItem>
-                                                <FormLabel className="text-sm text-muted-foreground">Электронная почта</FormLabel>
-                                                <FormControl>
-                                                    <Input className="w-full" placeholder="example@mail.ru" {...field} />
-                                                </FormControl>
-                                                <FormMessage />
-                                            </FormItem>
-                                        )}
-                                    />
-            </Form>
-        <div>{cardType?.description}</div>
-
+        <form >
+        <input className="w-full p-2" onChange={onTitleChange} placeholder="title" defaultValue={cardType.title} />
+        <div className='p-3'>{cardType?.description}</div>
+        </form>
         </div>
     </div>
   }
