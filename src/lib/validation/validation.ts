64e26f73a76090c34d0fe1dae4d862c9fb204030
Max-TeXnic <max.cyberia@gmail.com>
2025-04-21 18:36:03 +0800

    убрал валидацию с email

--- src/lib/validation/validation.ts
+++ src/lib/validation/validation.ts
@@ -5,7 +5,7 @@ export const formSchema = z.object({
   password: z.string().min(1, {
     message: "",
   }),
-  email: z.string().email({
+  email: z.string().min(1, {//.email({ // убрал пока валидацию с поля email, так как сейчас по логину авторизует
     message: "",
   }),
  // username: z.string().min(1, {
