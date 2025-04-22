a2b718205c8d3e342f8dda5780680886beb3fe0c
Устюжанин Антон Владимирович <anton177@mail.ru>
2025-04-22 12:16:34 +0800

    Добавил Дроп на фото пользователя

--- src/app/pages/userprofile/components/userData.tsx
+++ src/app/pages/userprofile/components/userData.tsx
@@ -2,14 +2,14 @@
 import Image from "next/image";
 import { useRef, useState, useEffect } from "react";
 import "../stylePageUP.css";
-import { Save} from "lucide-react";
+import { Save } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { any, z } from "zod";
-import  DialogAvatarEdit from "../Dialogs/DialogAvatarEdit"
-import {User } from "../types"
+import DialogAvatarEdit from "../Dialogs/DialogAvatarEdit"
+import { User } from "../types"
 import { Spinner } from '@/components/ui/spinner';
 import {
     Form,
@@ -37,11 +37,14 @@ export default function UserData({ userData
     let [curUser, setCurUser] = useState(userData);
     let [fotoHover, setfotoHover] = useState(false);
     let [linkImgAvatar, setLinkImgAvatar] = useState("")
+    let [dragClasses, setDragClasses] = useState("")
     // let [cardList, setCardList] = useState(cards || []); //вот так не работает. Почему так и непонял. Передал на мониторинг длины массива. Но это костыль как мне кажется.
     let [openDiagEditAvatar, setOpenDiagEditAvatar] = useState(false);
+    const allowedTypes = ["image/png", "image/jpeg"]
 
-
-    function saveAvatar (urlImg:string) {
+    function saveAvatar(urlImg: string) {
+        setfotoHover(false);
+         setDragClasses("")
         curUser.linkImg = urlImg
         setCurUser(curUser)
     }
@@ -66,9 +69,7 @@ export default function UserData({ userData
         console.log(event);
         const files: any[] = event.target.files;
         if (files.length > 0) {
-            const src = URL.createObjectURL(event.target.files[0])
-            setLinkImgAvatar(src)
-            setOpenDiagEditAvatar(true)
+            OpenDiagAvatar(files[0])
             //setCurUser(curUser)
             // console.log(files) ///img/userprofile/
             // alert(files[0].name);
@@ -80,47 +81,97 @@ export default function UserData({ userData
 
     // let [openDel, setOpenDel] = useState(false);
     function fotoMouseOver(event: any) {
-        setfotoHover((true));
+        setfotoHover(true);
         console.log("fotoHover", fotoHover);
     }
     function fotoMouseOut(evet: any) {
-        setfotoHover((false));
+        setfotoHover(false);
         console.log("fotoHover", fotoHover);
+    }
+
+    function dropHandler(ev: any): void {
+        console.log("File(s) dropped");
+
+        // Prevent default behavior (Prevent file from being opened)
+        ev.preventDefault();
+
+        if (ev.dataTransfer.items) {
+            // Use DataTransferItemList interface to access the file(s)
+            [...ev.dataTransfer.items].forEach((item, i) => {
+                // If dropped items aren't files, reject them
+                if (item.kind === "file") {
+                    const file = item.getAsFile();
+                    if (file && allowedTypes.includes(file.type)) {
+                        console.log(`… file[${i}].name = ${file.name}`);
+                        OpenDiagAvatar(file)
+                        return
+                    }
+
+                }
+            });
+        } else {
+            // Use DataTransfer interface to access the file(s)
+            [...ev.dataTransfer.files].forEach((file, i) => {
+                if (allowedTypes.includes(file.type)) {
+                    console.log(`… file[${i}].name = ${file.name}`);
+                    OpenDiagAvatar(file)
+                    return
+                }
+
+            });
+        }
+    }
+    function OpenDiagAvatar(file: File) {
+        const src = URL.createObjectURL(file)
+        setLinkImgAvatar(src)
+        setOpenDiagEditAvatar(true)
+    }
+    function dragOverHandler(event: any): void {
+        event.preventDefault();
+        setDragClasses("" ) //что-то не нравиться мне эта рамка outline-2 outline-primary-from
+        setfotoHover(true);
+    }
+    function dragLeaveHandler(event: any): void {
+        setfotoHover(false);
+        setDragClasses("")
     }
 
     return (
         <>
-        <DialogAvatarEdit open={openDiagEditAvatar} setOpen={setOpenDiagEditAvatar} saveAvatar={saveAvatar} urlImg={linkImgAvatar} />
-        <Form {...form}>
-                        <form
-                            onSubmit={form.handleSubmit(onSubmit)}
-                            className="flex h-full flex-col space-y-8"
-                        >
-                            <div className="mb-6">
-                                <h2 className="text-center text-xl">Редактировать профиль</h2>
-                            </div>
+            <DialogAvatarEdit open={openDiagEditAvatar} setOpen={setOpenDiagEditAvatar} saveAvatar={saveAvatar} urlImg={linkImgAvatar} />
+            <Form {...form}>
+                <form
+                    onSubmit={form.handleSubmit(onSubmit)}
+                    className="flex h-full flex-col space-y-8"
+                >
+                    <div className="mb-6">
+                        <h2 className="text-center text-xl">Редактировать профиль</h2>
+                    </div>
 
-                            <div className="mb-6 flex items-center justify-start gap-4">
-                                {/* <Camera size={60} /> */}
-                                {/* <div
+                    <div className="mb-6 flex items-center justify-start gap-4">
+                        {/* <Camera size={60} /> */}
+                        {/* <div
                                 style={{'--image-url': `url(${fetchedUrl})`} as React.CSSProperties}
                                 className='bg-[image:var(--image-url)]'>
                                     <!-- ... -->
                                 </div> */}
-                                <label
-                                    id="label-foto-user"
-                                    onMouseOver={fotoMouseOver}
-                                    onMouseOut={fotoMouseOut}
-                                    style={
-                                        {
-                                            "--image-url": `url(${curUser?.linkImg ? curUser.linkImg : "/img/userprofile/nofoto.svg"})`,
-                                        } as React.CSSProperties
-                                    }
-                                    htmlFor="userfoto"
-                                    className={`bg-[image:var(--image-url)] bg-cover ${!curUser?.linkImg && "bg-indigo-300"} relative size-[125] rounded-full border-indigo-300 hover:cursor-pointer`}
-                                >
-                                    {/* <label id="label-foto-user"  htmlFor="userfoto" className={`hover:opacity-[0.5] ${!curUser.linkImg&&'bg-indigo-300'} relative size-[125] rounded-full  border-indigo-300 hover:cursor-pointer`}> */}
-                                    {/* {
+                        <label
+                            id="label-foto-user"
+                            onMouseOver={fotoMouseOver}
+                            onMouseOut={fotoMouseOut}
+                            onDrop={dropHandler}
+                            onDragOver={dragOverHandler}
+                            onDragLeave={dragLeaveHandler}
+                            style={
+                                {
+                                    "--image-url": `url(${curUser?.linkImg ? curUser.linkImg : "/img/userprofile/nofoto.svg"})`,
+                                } as React.CSSProperties
+                            }
+                            htmlFor="userfoto"
+                            className={`bg-[image:var(--image-url)] bg-cover ${!curUser?.linkImg && "bg-indigo-300"} ${dragClasses} relative size-[125] rounded-full border-indigo-300 hover:cursor-pointer`}
+                        >
+                            {/* <label id="label-foto-user"  htmlFor="userfoto" className={`hover:opacity-[0.5] ${!curUser.linkImg&&'bg-indigo-300'} relative size-[125] rounded-full  border-indigo-300 hover:cursor-pointer`}> */}
+                            {/* {
                                     curUser.linkImg ?
                                     <Image
                                     className="rounded-full"
@@ -143,94 +194,94 @@ export default function UserData({ userData
                                 />
                                 } */}
 
-                                    {/* <span className="bg-[url(/img/userprofile/editWhite.svg)] border-2 border-black  bg-center size-[25]  bg-black top-25 left-20  rounded-full absolute  "></span> */}
-                                    {fotoHover ? (
-                                        <span className="absolute top-25 left-20 flex size-[30] items-center justify-center rounded-full bg-black">
-                                            <Image
-                                                className=" "
-                                                src="/img/userprofile/editWhite.svg"
-                                                alt="Profile Picture"
-                                                width={20}
-                                                height={20}
-                                                priority
-                                            />
-                                        </span>
-                                    ) : (
-                                        <span className="absolute top-25 left-20 flex size-[30] items-center justify-center rounded-full bg-white">
-                                            <Image
-                                                className=" "
-                                                src="/img/userprofile/edit.svg"
-                                                alt="Profile Picture"
-                                                width={20}
-                                                height={20}
-                                                priority
-                                            />
-                                        </span>
-                                    )}
-                                </label>
-                                <input
-                                    type="file"
-                                    hidden
-                                    className="w-0"
-                                    id="userfoto"
-                                    onChange={fotoChanged}
-                                    name="userfoto"
-                                ></input>
+                            {/* <span className="bg-[url(/img/userprofile/editWhite.svg)] border-2 border-black  bg-center size-[25]  bg-black top-25 left-20  rounded-full absolute  "></span> */}
+                            {fotoHover ? (
+                                <span className="absolute top-25 left-20 flex size-[30] items-center justify-center rounded-full bg-black">
+                                    <Image
+                                        className=" "
+                                        src="/img/userprofile/editWhite.svg"
+                                        alt="Profile Picture"
+                                        width={20}
+                                        height={20}
+                                        priority
+                                    />
+                                </span>
+                            ) : (
+                                <span className="absolute top-25 left-20 flex size-[30] items-center justify-center rounded-full bg-white">
+                                    <Image
+                                        className=" "
+                                        src="/img/userprofile/edit.svg"
+                                        alt="Profile Picture"
+                                        width={20}
+                                        height={20}
+                                        priority
+                                    />
+                                </span>
+                            )}
+                        </label>
+                        <input
+                            type="file"
+                            hidden
+                            className="w-0"
+                            id="userfoto"
+                            onChange={fotoChanged}
+                            name="userfoto"
+                        ></input>
 
-                                <div className="text-field">
-                                    <label htmlFor="name" className="block text-sm">
-                                        Имя пользователя:
-                                    </label>
+                        <div className="text-field">
+                            <label htmlFor="name" className="block text-sm">
+                                Имя пользователя:
+                            </label>
 
-                                    <div className="text-gray-500">{curUser?.login ? curUser.login : ""}</div>
-                                </div>
-                            </div>
+                            <div className="text-gray-500">{curUser?.login ? curUser.login : ""}</div>
+                        </div>
+                    </div>
 
-                            <div className="text-field mb-4">
-                                {/* <label htmlFor="email" className="block text-sm">Электронная почта</label>
+                    <div className="text-field mb-4">
+                        {/* <label htmlFor="email" className="block text-sm">Электронная почта</label>
                                     <div className="text-field__icon text-field__icon_email">
                                         <input type="email" id="email" name="email" required
                                             className="w-full p-2 mt-2 rounded-lg text-field__input"
                                             placeholder="Введите электронную почту" />
                                     </div> */}
 
-                                <FormField
-                                    control={form.control}
-                                    name="useremail"
-                                    render={({ field }) => (
-                                        <FormItem>
-                                            {/* <FormLabel>Электронная почта</FormLabel> */}
-                                            <label htmlFor="useremail" className="block text-sm">
-                                                Электронная почта
-                                            </label>
-                                            <FormControl>
-                                                <div className="text-field__icon text-field__icon_email">
-                                                    <Input
-                                                        className="text-field__input invalid:border-pink-500 invalid:text-pink-600"
-                                                        placeholder="Введите электронную почту"
+                        <FormField
+                            control={form.control}
+                            name="useremail"
+                            render={({ field }) => (
+                                <FormItem>
+                                    {/* <FormLabel>Электронная почта</FormLabel> */}
+                                    <label htmlFor="useremail" className="block text-sm">
+                                        Электронная почта
+                                    </label>
+                                    <FormControl>
+                                        <div className="text-field__icon text-field__icon_email">
+                                            <Input
+                                                className="text-field__input invalid:border-pink-500 invalid:text-pink-600"
+                                                placeholder="Введите электронную почту"
 
-                                                        {...field}
-                                                    />
-                                                </div>
-                                            </FormControl>
-                                            {/* <FormDescription>
+                                                {...field}
+                                            />
+                                        </div>
+                                    </FormControl>
+                                    {/* <FormDescription>
 
                                                 </FormDescription> */}
-                                            <FormMessage className="text-[0.8rem]" />
-                                        </FormItem>
-                                    )}
-                                />
-                            </div>
-                            <div className="flex grow-1 items-end justify-end">
-                                <Button type="submit" className="bg-gradient">
-                                    {" "}
-                                    <Save className="size-7" />
-                                    <span>Сохранить</span>
-                                </Button>
-                                {/* </fieldset> */}
+                                    <FormMessage className="text-[0.8rem]" />
+                                </FormItem>
+                            )}
+                        />
+                    </div>
+                    <div className="flex grow-1 items-end justify-end">
+                        <Button type="submit" className="bg-gradient">
+                            {" "}
+                            <Save className="size-7" />
+                            <span>Сохранить</span>
+                        </Button>
+                        {/* </fieldset> */}
 
-                                {/* <Button className="bg-gradient"> <Image src="/img/UserProfile/saveIcon.svg" alt="github logo" width={20} height={20} /><span>Сохранить</span></Button> */}
-                                {/* <button className="bg-gradient text-white py-1 px-3  rounded-md ">
+                        {/* <Button className="bg-gradient"> <Image src="/img/UserProfile/saveIcon.svg" alt="github logo" width={20} height={20} /><span>Сохранить</span></Button> */}
+                        {/* <button className="bg-gradient text-white py-1 px-3  rounded-md ">
                             <svg className="icon-button" width="20" height="20"
                                 viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path
@@ -240,9 +291,9 @@ export default function UserData({ userData
                             <span>Сохранить</span>
 
                         </button> */}
-                            </div>
-                        </form>
-                    </Form>
+                    </div>
+                </form>
+            </Form>
         </>
     );
 }
