b153c9edc098e746142c0d99c8bdd021ee69375e
Max-TeXnic <max.cyberia@gmail.com>
2025-04-22 12:31:48 +0800

    revert

--- src/app/cardtypes/[slug]/page.tsx
+++ src/app/cardtypes/[slug]/page.tsx
@@ -16,6 +16,8 @@ import {
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Button } from "@/components/ui/button"
+import { title } from 'node:process';
+import { Description } from '@radix-ui/react-dialog';
 
 export default function Page({ params }: { params: Promise<{ slug: string }>}) {
     const [cardType, setCardType] = React.useState<CardTypeEntity>({id:0,title:''});
@@ -30,21 +32,40 @@ export default function Page({ params }: { params: Promise<{ slug: string }>}) {
         };
             fetchData();
     },[]);
-    const {register, handleSubmit} = useForm();
-    const onTitleChange = (e)=>{
-        console.log(e);
-    }
+    // const onTitleChange = (e)=>{
+    //     console.log(e.target.value)
+    //     const tmp = cardType
+    //     tmp.title = e.target.value
+    //     console.log(tmp)
+    //     setCardType(tmp)
+    // }
 
+    const form = useForm<CardTypeEntity>();
 
     return <div className="h-screen font-sans overflow-y-auto p-20">
     <div className="bg-white text-gray-800 rounded-lg w-full sm:h-3/4 sm:min-w-xl overflow-y-auto mb-2 sm:mb-0">
         <div className="flex items-center rounded-t-lg  h-15 px-8 bg-header-project-list">
-            <h3 className="text-xl text-white">{cardType?.title}</h3>
+                <h3 className="text-xl text-white">{cardType.title}</h3>
         </div>
-        <form >
+            {/* <form >
         <input className="w-full p-2" onChange={onTitleChange} placeholder="title" defaultValue={cardType.title} />
         <div className='p-3'>{cardType?.description}</div>
-        </form>
+            </form> */}
+            <Form {...form}>
+                <FormField
+                    control={form.control}
+                    name="title"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel className="text-sm text-muted-foreground">Название</FormLabel>
+                            <FormControl>
+                                <Input className="w-full" placeholder="title" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+            </Form>
         </div>
     </div>
   }
