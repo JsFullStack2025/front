4221cba4609a69c1c7eaa659f1b7e66184f423a1
IlyaA <ilya0000007@gmail.com>
2025-04-24 13:22:15 +0700

    мобильная версия

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,19 +35,19 @@ export default function welcome() {
 
 
     return (
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 sm:p-20">
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 sm:p-0">
 
             <main className="flex flex-col gap-30 row-start-2 items-center sm:items-start">
                 <div className="grid grid-rows gap-10 justify-items-center sm:items-start">
                     <Card className=" white-on-small-width flex h-[100%] w-full items-center justify-between gap-8 px-8 lg:flex-row  bg-gradient-to-r from-white via-white via-40% via-transparent via-75%">
-                        <p className=" basis-1/3 font-jet text-6xl text-center text-balance text-transparent bg-gradient-to-r from-primary-from to-primary-to bg-clip-text">
+                        <p className=" basis-1/3 font-jet text-5xl fold-on-small-width text-center text-balance text-transparent bg-gradient-to-r from-primary-from to-primary-to bg-clip-text">
                             Создавайте кастомизируйте и делитесь своими визитками с помощью VISITEO. </p>
                         <Image
-                            className="shadow-xl rounded-md"
+                            className="shadow-xl fold-on-small-width rounded-md"
                             src="/card.png"
                             alt="CardExample"
                             width={400}
-                            height={700} />
+                            height={585} />
                     </Card>
                     <motion.button className="bg-white h-15 rounded-md gap-2 px-5 has-[>svg]:px-7"
                         whileHover={{
@@ -55,7 +55,7 @@ export default function welcome() {
                             transition: { duration: 0.4 },
                         }}
                         whileTap={{ scale: 0.9 }}>
-                        <Link href="/login"> <p className="text-3xl text-center text-balance gradient font-jet p-1 ">
+                        <Link href="/login"> <p className=" fold-on-small-width text-3xl text-center text-balance gradient font-jet p-1 ">
                             Зарегистрируйтесь бесплатно</p></Link> 
                         </motion.button>
 
@@ -69,7 +69,7 @@ export default function welcome() {
                 </div>
 
 
-                <motion.div
+               <motion.div
                     initial="hidden"
                     whileInView="visible"
                     viewport={{ margin: "-100px +900px -100px +900px" }}
@@ -79,14 +79,14 @@ export default function welcome() {
                 >
                     <Card className="white-on-small-width flex h-[100%] w-full items-center justify-between gap-8 px-8 lg:flex-row  bg-gradient-to-r via-transparent via-20% via-white via-60% to-white">
                         <motion.img
-                            className="shadow-xl rounded-md"
+                            className="shadow-xl fold-on-small-width rounded-md"
                             src="/dragndrop.png"
                             alt="Drag'n'drop"
                             width={350}
                             height={350}
 
                         />
-                        <p className="p-3 basis-1/3 font-jet text-5xl text-center text-balance text-transparent bg-gradient-to-r from-primary-from to-primary-to bg-clip-text">
+                        <p className="p-3 basis-1/3 font-jet text-5xl fold-on-small-width text-center text-balance text-transparent bg-gradient-to-r from-primary-from to-primary-to bg-clip-text">
                             Удобный редактор с функцией Drag'n'Drop
                         </p>
                     </Card>
@@ -99,13 +99,13 @@ export default function welcome() {
                     variants={divAnimation}>
                     <Card className="white-on-small-width flex h-[100%] w-full items-center justify-between gap-8 px-8 lg:flex-row  bg-gradient-to-r via-transparent via-20% via-white via-60% to-white">
                         <motion.img
-                            className="shadow-xl rounded-md"
+                            className="shadow-xl fold-on-small-width rounded-md"
                             src="/template.png"
                             alt="template"
                             width={600}
                             height={400}
                         />
-                        <p className=" p-3 basis-1/3 font-jet text-5xl text-center text-balance text-transparent bg-gradient-to-r from-primary-from to-primary-to bg-clip-text">
+                        <p className=" p-3 basis-1/3 font-jet text-5xl fold-on-small-width text-center text-balance text-transparent bg-gradient-to-r from-primary-from to-primary-to bg-clip-text">
                             Настраеваемые шаблоны помогут создать визитки на все случаи жизни
                         </p>
                     </Card>
@@ -118,16 +118,16 @@ export default function welcome() {
                     variants={divAnimation}>
                     <Card className="p-3 white-on-small-width flex h-[100%] w-full items-center justify-between gap-8 px-8 lg:flex-row  bg-gradient-to-r via-transparent via-20% via-white via-60% to-white">
                         <Image
-                            className="shadow-xl rounded-md"
+                            className="shadow-xl fold-on-small-width rounded-md"
                             src="/share.jpg"
                             alt="template"
                             width={300}
                             height={300}
                         />
-                        <p className=" p-3 basis-1/3 font-jet text-5xl text-center text-balance text-transparent bg-gradient-to-r from-primary-from to-primary-to bg-clip-text">
+                        <p className=" p-3 basis-1/3 font-jet text-5xl fold-on-small-width text-center text-balance text-transparent bg-gradient-to-r from-primary-from to-primary-to bg-clip-text">
                             Делитесть своими карточками как вам удобно и где удобно </p>
                     </Card>
-                </motion.div>
+                </motion.div> 
             </main>
 
 
