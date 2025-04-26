63767639d80e72130b2ce1c797e700bc646cf981
ClausCode <nclaus.code@gmail.com>
2025-04-26 18:04:10 +0300

    feat/header: added DashboardButton

--- src/widgets/app-header/_ui/account-button.tsx
+++ src/widgets/app-header/_ui/account-button.tsx
@@ -2,6 +2,9 @@
 
 import Link from "next/link"
 
+import { User } from "@/features/auth/_types/user.types"
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/shared/ui/avatar"
 import { Button } from "@/shared/ui/button"
 
 import { useUser } from "@/shared/hooks/useUser.hook"
@@ -10,11 +13,20 @@ export function AccountButton() {
 	const { user, isLoading } = useUser()
 
 	if (isLoading || !user) return <AuthButton />
-	return "Avatar"
+	return <DashboardButton user={user} />
 }
 
 const AuthButton = () => (
 	<Link href="/auth/signin">
 		<Button>Вход</Button>
+	</Link>
+)
+
+const DashboardButton = ({ user }: { user: User }) => (
+	<Link href="/dashboard">
+		<Avatar>
+			<AvatarImage src={user.picture} />
+			<AvatarFallback>{user.displayName.slice(0, 1)}</AvatarFallback>
+		</Avatar>
 	</Link>
 )
