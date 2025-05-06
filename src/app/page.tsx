4a3b168ba8e32aa5d91be4805108ed1a810e99ab
Oleg Kulik <kulik.ol.val@gmail.com>
2025-05-06 23:00:40 +0700

    logo google

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center  p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         this is main page
-        <Link href={`${process.env.BACKEND_URI}/auth/google`}> ddf </Link>
+        <Link href={`${process.env.API_URL}/auth/google`}> ddf </Link>
       </main>
     </div>
   );
