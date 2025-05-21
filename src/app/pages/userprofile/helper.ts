7d153801215709ebb7a650967f01da9dee3d9941
Oleg Kulik <kulik.ol.val@gmail.com>
2025-05-21 07:34:23 +0300

    .env.example

--- src/app/pages/userprofile/helper.ts
+++ src/app/pages/userprofile/helper.ts
@@ -2,6 +2,8 @@ import { cookies } from "next/headers"
 
 export async function getUserIdFromCookie ():Promise<number> {
     let userAuth = (await cookies()).get("auth-cookie")?.value
+    
   const UserId:number = JSON.parse(userAuth?.replace("j:","") || '[]').userId
+  console.log("userID", UserId)
   return UserId
 }
